522eade1657a29fa482b8009f303d55eed9d7172
DanaGriff <DanaGriff1201@gmail.com>
2018-11-20 01:32:05 +0200

    print calendar id when no events found

--- color_coding_script.py
+++ color_coding_script.py
@@ -127,7 +127,7 @@ def main(data):
 
 
         if not events:
-            print('No upcoming events found.')
+            print('{0} {1}'.format('No upcoming events found for ', calendar_id))
 
         for event in events:
             counter += color_event(event, data, service, calendar_id)
